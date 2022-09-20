1b942960230f4f0bf4937ed7e55cb67e309d731c
Adrian <adrian.widz95@gmail.com>
2022-09-20 08:50:00 -0400

    added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,37 @@
+package com.example.springpetclinic.services.map;
+
+import com.example.springpetclinic.model.PetType;
+import com.example.springpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findByID(id);
+    }
+}
