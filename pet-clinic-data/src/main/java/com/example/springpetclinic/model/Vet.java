0c0e71f8c4ab65baa5bcd9b19ac2818789c6ce76
Adrian <adrian.widz95@gmail.com>
2022-09-20 09:21:46 -0400

    Added Pets and Contact info to owners on startup. Closes #33

--- pet-clinic-data/src/main/java/com/example/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/model/Vet.java
@@ -1,10 +1,15 @@
 package com.example.springpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Vet extends Person {
 
     private Set<Speciality> specialities;
+
+    public Vet() {
+        this.specialities = new HashSet<>();
+    }
 
     public Set<Speciality> getSpecialities() {
         return specialities;
