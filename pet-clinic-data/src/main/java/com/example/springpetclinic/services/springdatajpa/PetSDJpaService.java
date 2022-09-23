494aef00cfe833942523961b61032c4fb824e483
Adrian <adrian.widz95@gmail.com>
2022-09-23 09:11:09 -0400

    added spring data jpa service for Speciality. Closes #44

--- pet-clinic-data/src/main/java/com/example/springpetclinic/services/springdatajpa/PetSDJpaService.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -3,10 +3,14 @@ package com.example.springpetclinic.services.springdatajpa;
 import com.example.springpetclinic.model.Pet;
 import com.example.springpetclinic.repositories.PetRepository;
 import com.example.springpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class PetSDJpaService implements PetService {
 
     private final PetRepository petRepository;
