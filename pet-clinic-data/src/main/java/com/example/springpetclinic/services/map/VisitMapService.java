9e3290b282da9d08144a43190cb76f3a6d77b1cb
Adrian <adrian.widz95@gmail.com>
2022-09-26 12:01:12 -0400

    Implement Map imp for Visit Service

---
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package com.example.springpetclinic.services.map;
+
+import com.example.springpetclinic.model.Visit;
+import com.example.springpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbbstractServiceMap<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findByID(id);
+    }
+}
